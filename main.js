2d1a0579b062fe269db15276871d30ff0d5b5cf4
Elijah <bundielly@gmail.com>
2022-06-15 21:19:28 +0300

    Assignment complete

--- main.js
+++ main.js
@@ -3,8 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// first make a Nodelist of all likes
+// add a button for each of the likes
+// the button will turn to FULL_HEART when in empty
+// and back to EMPTY_HEART when in full.
+
+// remove the error message by introducing 'hidden.'
 
 
+
+const modal = document.querySelector('#modal')
+modal.classList = 'hidden'
+// console.log(modal)
+
+const likeys = document.querySelectorAll('.like-glyph')
+// console.log(likeys)
+for (let like of likeys) {
+  like.addEventListener('click', simpleLiker)
+}
+
+function simpleLiker() {
+  mimicServerCall()
+  .then(() => {
+    if (like.textContent === EMPTY_HEART) {
+      like.textContent = FULL_HEART
+      like.classList.add('activated-heart')
+    } 
+    else if (like.textContent === FULL_HEART) {
+      like.textContent = EMPTY_HEART
+      like.classList.remove('activated-heart')
+    }
+  })
+  .catch(() => {
+    modal.classList.remove('hidden')
+    modal.textContent = 'Error!'
+    setTimeout(() => {
+      modal.classList.add('hidden')
+    }, 3000)
+  })
+}
 
 
 //------------------------------------------------------------------------------
